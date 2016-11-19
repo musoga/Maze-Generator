37f6d6243aebf2380f8fbe30e44ccf2bbdb40c6c
Robert Campbell <robertgcampbell95@gmail.com>
2016-11-18 21:43:27 -0800

    Add junit test file. Implement thoughts for tests also

---
+++ MazeTest.java
@@ -0,0 +1,70 @@
+package sjsu.cs146.project3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MazeTest {
+	/*
+
+	@Before
+	public void setUp() throws Exception {
+		int height = 5;
+		int width = 5;
+		Maze maze = new Maze(height, width);
+	}
+	
+	@Test
+	public void testSetSeed() {
+		int seed = 0;
+		int height = 5;
+		int width = 5;
+		
+		Maze maze1 = new Maze(5, 5);
+		maze1.setSeed(seed);
+		maze1.generateMaze();
+		
+		Maze maze2 = new Maze(5, 5);
+		maze2.setSeed(seed);
+		maze2.generateMaze();
+		
+		Node[] adjacencyList1 = maze_1.getMaze();
+		Node[] adjacencyList2 = maze_2.getMaze();
+		
+		for(int index = 0;index < adjacencyList1.size;index++) {
+			Node current1 = adjacencyList1[index];
+			Node current2 = adjacencyList1[index];
+			
+			while(current1 != null && current2 != null) {
+				assertEquals(current1.getValue(), current2.getValue());
+				
+				current1 = current1.next;
+				current2 = current2.next;
+			}
+		}
+	}
+	
+	@Test
+	public void testGenerateMaze() {
+		
+	}
+	
+	@Test
+	public void testSearchMazeBFS() {
+		
+	}
+	
+	@Test
+	public void testSearchMazeDFS() {
+		
+	}
+	
+	@Test
+	public void testPrintMaze() {
+		
+	}
+	
+	*/
+
+}
